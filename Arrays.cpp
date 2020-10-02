4b239938baf3818bc36a9a88a16dc9b8b6e2db94
pn6703 <71823258+pn6703@users.noreply.github.com>
2020-10-02 12:15:28 +0530

    Update Arrays.cpp

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,10 +1,8 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-   int a[5];
-   for(int i=0;i<5;i++)
-   {
-     cout<<a[i]
-   }
+   int a[5]=[1,2,3,5,6];// also intialise values.
+   for(int i=0;i<5;i++)//in dynamic programing you can also perform loops without {} braces.It reduces time complexity.
+     cout<<a[i];
 return 0;
 }
