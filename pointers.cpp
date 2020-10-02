bd69e554f4ce775a7ce212644721a518532dc9b9
pn6703 <71823258+pn6703@users.noreply.github.com>
2020-10-02 11:59:51 +0530

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -4,10 +4,10 @@ int main()
 {
   int a;
   int *p;
-  p=&a;
-  cin>>a;
-  cout<<a;
-  cout<<*p<<endl;
-
+  p=&a; //accesing address of a to pointer p
+  cin>>a;// assume a=10
+  cout<<a;// this will give you a value of a i.e '10'.
+  cout<<*p<<endl; // this will also give you a value of a i.e '10'.
+  cout<<p<<endl; // compiler will give you address of a.
 return 0;
 }
